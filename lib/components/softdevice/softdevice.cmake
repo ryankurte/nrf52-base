eedf54a6f4030c0f5eeeba2e58a8b215394f3644
Ryan Kurte <ryankurte@gmail.com>
2015-07-10 18:59:07 +1200

    Updates to softdevice file

--- lib/components/softdevice/softdevice.cmake
+++ lib/components/softdevice/softdevice.cmake
@@ -1,7 +1,25 @@
 # Softdevice inclusion
 
+include_directories(${CMAKE_CURRENT_LIST_DIR}/common/softdevice_handler)
+
+set(SOFTDEVICE_SOURCES
+	${CMAKE_CURRENT_LIST_DIR}/common/softdevice_handler/softdevice_handler_appsh.c
+	${CMAKE_CURRENT_LIST_DIR}/common/softdevice_handler/softdevice_handler.c
+)
+
 if(${SOFTDEVICE} EQUAL "S132")
+
+include_directories(${CMAKE_CURRENT_LIST_DIR}/s132/headers)
+
+set(SOFTDEVICE_LD ${CMAKE_CURRENT_LIST_DIR}/s132/toolchain/armgcc/armgcc_s132_nrf52832_xxaa.ld)
 
 elseif(${SOFTDEVICE} EQUAL "S212")
 
+include_directories(${CMAKE_CURRENT_LIST_DIR}/s212/headers)
+
+set(SOFTDEVICE_LD ${CMAKE_CURRENT_LIST_DIR}/s212/toolchain/armgcc/armgcc/s21_nrf52832_xxaa.ld)
+
 endif()
+
+add_library(softdevice ${SOFTDEVICE_SOURCES})
+
